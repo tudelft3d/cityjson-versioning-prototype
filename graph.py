cff246464c6c84162076d45d24cc55cbbd8cd02e
Stelios Vitalis <steliosvitalis@gmail.com>
2022-07-11 16:48:32 +0200

    Recover full functionality of log graph and refactor log commands

--- graph.py
+++ graph.py
@@ -39,8 +39,8 @@ class History:
         """Returns the dag object."""
         return self._dag
 
-class SimpleHistoryLog:
-    """Class to print a simple list log of a History."""
+class AbstractLog:
+    """Abstract class that shares logic related to formatting of logs"""
 
     def __init__(self, history: 'History'):
         self._history = history
@@ -52,6 +52,30 @@ class SimpleHistoryLog:
             "message": "white",
             "main": "white"
         })
+    
+    def get_header_text(self, version):
+        """Returns the header line of a version, formatted."""
+        header_line = f"[header]version {version.name}"
+
+        if len(version.branches) > 0:
+            branches_txt = self.get_refs_string(version.branches, "branch")
+            header_line += f" ({branches_txt})"
+
+        if len(version.tags) > 0:
+            tags_txt = self.get_refs_string(version.tags, "tag")
+            header_line += f" ({tags_txt})"
+
+        header_line += "[/header]"
+
+        return header_line
+    
+    def get_refs_string(self, refs, ref_type):
+        """Returns a string with comma-seperated names of the refs."""
+        return ", ".join([f"[{ref_type}]{ref}[/{ref_type}]"
+                          for ref in refs])
+
+class SimpleHistoryLog(AbstractLog):
+    """Class to print a simple list log of a History."""
 
     def print_all(self):
         """Prints the history as a list."""
@@ -70,17 +94,7 @@ class SimpleHistoryLog:
 
         console = Console(theme=self._theme, highlight=False)
 
-        header_line = f"[header]version {version.name}"
-
-        if len(version.branches) > 0:
-            branches_txt = self.get_refs_string(version.branches, "branch")
-            header_line += f" ({branches_txt})"
-
-        if len(version.tags) > 0:
-            tags_txt = self.get_refs_string(version.tags, "tag")
-            header_line += f" ({tags_txt})"
-
-        header_line += "[/header]"
+        header_line = self.get_header_text(version)
 
         console.print(Padding(header_line, (0, 0)))
 
@@ -91,74 +105,9 @@ class SimpleHistoryLog:
 
         console.print(Padding(msg, (1, 4)))
 
-    def get_refs_string(self, refs, ref_type):
-        """Returns a string with comma-seperated names of the refs."""
-        return ", ".join([f"[{ref_type}]{ref}[/{ref_type}]"
-                          for ref in refs])
-
-class GraphHistoryLog:
+    
+class GraphHistoryLog(AbstractLog):
     """Class to print a history with a graph."""
-
-    def __init__(self, history: 'History'):
-        self._history = history
-
-        self._colors = {
-            "header": Fore.YELLOW,
-            "branch": Fore.CYAN,
-            "tag": Fore.MAGENTA,
-            "message": Fore.CYAN,
-            "main": Fore.WHITE
-        }
-
-        self._theme = Theme({
-            "header": "yellow",
-            "branch": "cyan",
-            "tag": "magenta",
-            "message": "white",
-            "main": "white"
-        })
-
-        self._all_branches = []
-        self._active_branch = 0
-        self._branches_shown = []
-        self._divide = False
-        self._merge = False
-
-    def update_status(self, version_name):
-        """Updates the status of the graph (calculates if this is a merge,
-        divide, which branch is etc.).
-        """
-        # Find in which branches exists this version name
-        in_branches = [i for i, b in enumerate(self._all_branches)
-                       if version_name in b]
-
-        self._divide = False
-        self._merge = False
-
-        if len(in_branches) == 1:
-            # If only one branch is found, then this is nothing special,
-            # no divide or merge.
-            self._active_branch = in_branches[0]
-
-            if self._all_branches[in_branches[0]] not in self._branches_shown:
-                self._branches_shown.append(self._all_branches[in_branches[0]])
-        else:
-            # If this exists in two branches, it is either a divide or a merge
-            # version.
-            self._active_branch = min(in_branches)
-
-            # Is this a divide version?
-            for b in in_branches:
-                # If this is not in the existing shown branches, it's a divide
-                # version.
-                if self._all_branches[b] not in self._branches_shown:
-                    self._branches_shown.append(self._all_branches[b])
-                    self._divide = True
-
-            # If this is not a divide version, then it's a merge version.
-            if not self._divide:
-                self._branches_shown.remove(self._all_branches[in_branches[1]])
-                self._merge = True
 
     def print_all(self):
         """Prints the history as a graph."""
@@ -190,6 +139,7 @@ class GraphHistoryLog:
 
         active_paths = [all_paths[0]]
         for version_name in sorted_keys:
+            version = self._history.citymodel.versioning.versions[version_name]
             found = False
             header_line = ""
 
@@ -219,7 +169,7 @@ class GraphHistoryLog:
 
                 header_line += "| "
 
-            header_line += f"[header]{version_name}[/header]"
+            header_line += self.get_header_text(version)
 
             console.print(header_line)
 
@@ -239,75 +189,3 @@ class GraphHistoryLog:
             msg = indent(fill(version.message, 70, initial_indent="[message]    ", subsequent_indent="[message]    "), lines)
             console.print(msg)
             console.print(lines)
-
-            # current_version = cm.versioning.versions[version_name]
-
-            # self.print_version(current_version)
-
-    def print_version(self, version: 'Version'):
-        """Prints a given version."""
-        colors = self._colors
-        lines_txt = self.get_lines_of_branches()
-
-        if self._merge:
-            print("|/")
-
-        print(self.get_branch_lines_with_star(self._active_branch), end='')
-        print(colors["header"], end='')
-        print("  version {name}".format(name=version.name), end='')
-
-        if len(version.branches) > 0:
-            branches_txt = self.get_refs_string(version.branches,
-                                                colors["branch"],
-                                                colors["header"])
-            print(' ({branches})'.format(branches=branches_txt), end='')
-
-        if len(version.tags) > 0:
-            tags_txt = self.get_refs_string(version.tags,
-                                            colors["tag"],
-                                            colors["header"])
-            print(' ({tags})'.format(tags=tags_txt), end='')
-
-        print(colors["main"])
-
-        if self._divide:
-            print("|\\", end='')
-        else:
-            print(lines_txt, end='')
-        print("  Author: {author}".format(author=version.author))
-        print(lines_txt, end='')
-        print("  Date: {date}".format(date=version.date))
-
-        print(lines_txt, end='')
-        msg = ("  Message:\n"
-               "{l}\n"
-               "{}{l}\t{}{}\n"
-               "{l}\n").format(Fore.CYAN,
-                               ('\n' + lines_txt + '\t').join(version.message.splitlines(False)),
-                               Style.RESET_ALL,
-                               l=lines_txt)
-        print(msg, end='')
-
-    def get_branch_lines_with_star(self, branch_i):
-        """Returns a string with lines according to branches status."""
-        if self._divide or self._merge:
-            times = len(self._branches_shown) - 1
-        else:
-            times = len(self._branches_shown)
-
-        result = "".join(['| ' for i in range(branch_i)])
-        result += "* "
-        result += "".join(['| ' for i in range(branch_i + 1, times)])
-
-        return result
-
-    def get_lines_of_branches(self):
-        """Returns the text with lines for the currently shown branchs."""
-        return ''.join(['| ' for i in range(len(self._branches_shown))])
-
-    def get_refs_string(self, refs, ref_color, reset_color):
-        """Returns a string with comma-seperated names of the refs."""
-        return ", ".join(["{}{}{}".format(ref_color,
-                                          ref,
-                                          reset_color)
-                          for ref in refs])
